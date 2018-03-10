e8096851e1c1dff6b97478572d016c50a44a6827
Norbert Csongradi <norbert@csongradi.hu>
2018-03-10 21:59:27 +0100

    Added static file serving, index.html, very basic CSS

--- main.go
+++ main.go
@@ -7,17 +7,37 @@ import (
 	"net/http"
 	"fmt"
 	"strconv"
+	"flag"
+	"time"
 )
 
 const PORT = 3000;
 
 func main() {
+	var dir string;
+
+	flag.StringVar(&dir, "dir", "./static", "the directory to serve files from. Defaults to the ./static dir");
+    flag.Parse();
+
 	router := mux.NewRouter();
 
 	router.HandleFunc("/ping", PingHandler);
 
+	// serve static files
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))));
+	router.HandleFunc("/", IndexHandler);
+
 	fmt.Println("Starting http server on http://localhost:", PORT);
-	http.ListenAndServe(":"+strconv.Itoa(PORT), handlers.LoggingHandler(os.Stdout, router));
+
+	srv := &http.Server {
+        Handler:      handlers.LoggingHandler(os.Stdout, router),
+        Addr:         ":"+strconv.Itoa(PORT),
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+
+	};
+
+	srv.ListenAndServe();
 }
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +46,8 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK);
 
     fmt.Fprintf(w, "Pong")
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/static/index.html", http.StatusMovedPermanently);
 }
